6f3d490cdcc91e6866111631474d95fd94337835
Sullivan <adressecours0.1@gmail.com>
2023-03-28 15:47:46 +0200

    text content of the main calculator body

--- app.js
+++ app.js
@@ -22,6 +22,59 @@ mainBodyCalculator.setAttribute('id','mainBody')
 
 calculator.append(display, mainBodyCalculator)
 
+let one = document.createElement('div')
+let two = document.createElement('div')
+let three = document.createElement('div')
+let four = document.createElement('div')
+let five = document.createElement('div')
+let six = document.createElement('div')
+let seven = document.createElement('div')
+let eight = document.createElement('div')
+let nine = document.createElement('div')
+let zero = document.createElement('div')
+let addButton = document.createElement('div')
+let subtractButton = document.createElement('div')
+let divideButton = document.createElement('div')
+let multiplyButton = document.createElement('div')
+let clear = document.createElement('div')
+let equal = document.createElement('div')
+
+one.setAttribute('class','button')
+two.setAttribute('class','button')
+three.setAttribute('class','button')
+four.setAttribute('class','button')
+five.setAttribute('class','button')
+six.setAttribute('class','button')
+seven.setAttribute('class','button')
+eight.setAttribute('class','button')
+nine.setAttribute('class','button')
+zero.setAttribute('class','button')
+addButton.setAttribute('class','button')
+subtractButton.setAttribute('class','button')
+divideButton.setAttribute('class','button')
+multiplyButton.setAttribute('class','button')
+clear.setAttribute('class','button')
+equal.setAttribute('class','button')
+
+one.textContent = '1';
+two.textContent = '2';
+three.textContent = '3';
+four.textContent = '4';
+five.textContent = '5';
+six.textContent = '6';
+seven.textContent = '7';
+eight.textContent = '8';
+nine.textContent = '9';
+zero.textContent = '0';
+addButton.textContent = '+';
+subtractButton.textContent = '-';
+divideButton.textContent = '/';
+multiplyButton.textContent = '*'
+clear.textContent = 'Clear';
+equal.textContent = '=';
+
+
+mainBody.append(one,two,three,four,five,six,seven,eight,nine,zero,addButton,subtractButton,divideButton,multiplyButton,clear,equal)
 
 let number1;
 let number2;
