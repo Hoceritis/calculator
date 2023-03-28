82ab539049f27491688c9a809632359bc555221b
Sullivan <adressecours0.1@gmail.com>
2023-03-28 10:49:41 +0200

    functions for math operators created

--- app.js
+++ app.js
@@ -9,3 +9,25 @@ title.setAttribute('id','title')
 mainDiv.appendChild(title)
 
 title.textContent =" JS calculator "
+
+
+function add (number1, number2){
+    let result = number1 + number2;
+      return result
+  } 
+
+  function subtract (number1, number2){
+    let result = number1 - number2;
+      return result
+  } 
+
+  function multiply (number1, number2){
+    let result = number1 * number2;
+      return result
+  } 
+
+    function divide (number1, number2){
+    let result = number1 / number2;
+      return result
+  } 
+
